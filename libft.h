77b1998b94fcf6d04a789449d9ba5e968ac0de5d
Ellie <102157178+Otazka@users.noreply.github.com>
2024-05-27 00:39:18 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -6,35 +6,81 @@
 /*   By: elsurovt <elsurovt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 03:20:09 by elsurovt          #+#    #+#             */
-/*   Updated: 2024/05/26 03:30:33 by elsurovt         ###   ########.fr       */
+/*   Updated: 2024/05/27 00:38:42 by elsurovt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
-#include <stdlib.h>
-#include <unistd.h>
+# define LIBFT_H
 
-int ft_atoi(const char *str);
-void ft_bzero(void *s, size_t a);
-int ft_isalnum(int a);
-int ft_isalpha(int a);
-int ft_isascii(int a);
-int ft_isdigit(int a);
-int ft_isprint(int a);
-void *ft_memchr(const void *str, int a, size_t b);
-int ft_memcmp(const void *str1, const void *str2, size_t a);
-void *ft_memcpy(void *dst, const void *src, size_t a);
-void *ft_memmove(void *dst, const void *src, size_t a);
-void *ft_memset(void *str, int a, size_t b);
-char *ft_strchr(const *str, int a);
-size_t strlcat(char *dst, const char *src, size_t dstsize);
-size_t ft_strlcpy(char *dst, const char *src, size_t siz);
-int ft_strlen(const char *str);
-int ft_strncmp(const char *s1, const char *s2, size_t a);
-char *ft_strnstr(const char *big, const char *little, size_t len);
-char *ft_strrchr(const char *str, int a);
-int ft_tolower(int a);
-int ft_toupper(int a);
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+//------------LIBFT---------------/
+// Mandatory Part 1:
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_atoi(const char *ptr);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strdup(const char *str);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+void	ft_bzero(void *buffer, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memset(void *buffer, int c, size_t n);
+void	*ft_memchr(const void *str, int c, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+// Mandatory Part 2:
+char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char	*ft_substr(char const *str, unsigned int start, size_t len);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+//Part Bonus:
+typedef struct s_list
+{
+    void			*content;
+    struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+//------------GET_NEXT_LINE---------------/
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# ifndef MAX_F_OPENED
+#  define MAX_F_OPENED 1024
+# endif
+
+char	*get_next_line(int fd);
 
 #endif
