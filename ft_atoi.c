be9ecc1529eb95f813c6526cc46305b028a36c47
Ellie <102157178+Otazka@users.noreply.github.com>
2024-05-28 16:38:22 +0200

    all

--- ft_atoi.c
+++ ft_atoi.c
@@ -30,7 +30,7 @@ int ft_atoi(const char *str)
     }
     else if (str[i] == '+')
         i++;
-    while (str[i] != '/0')
+    while (str[i])
     {
         if (str[i] >= '0' && str[i] <= '9')
             base = base * 10 + str[i] - '0';
