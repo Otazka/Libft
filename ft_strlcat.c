a8d1e43f79c3eec7924fc77903fb5037821f5336
Ellie <102157178+Otazka@users.noreply.github.com>
2024-05-28 16:36:26 +0200

    all

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,1 +1,39 @@
-/* *****************
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elsurovt <elsurovt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 01:38:13 by elsurovt          #+#    #+#             */
+/*   Updated: 2024/05/26 02:02:06 by elsurovt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+    size_t i;
+    size_t len_dst;
+    size_t len_src;
+    size_t a;
+    size_t b;
+
+    if ((dst == NULL) && (dstsize == 0))
+        return (0);
+    len_dst = ft_strlen(dst);
+    len_src = ft_strlen(src);
+    if (dstsize < len_dst)
+        return (ft_strlen(src) + dstsize);
+    i = 0;
+    a  = dstsize - len_dst;
+    b = len_dst + len_src;
+    while (((i + 1) < n) && (*(src + i) != '\0'))
+    {
+        *(dest + (len_dst + i)) = *(src + i);
+        i++;
+    }
+    *(dst + (len_dst + i)) = '\0';
+    return (b);
+}
--- ft_substr.c
+++ ft_substr.c
@@ -1,2 +1,33 @@
 /* ************************************************************************** */
-/*        
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elsurovt <elsurovt@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 22:32:10 by elsurovt          #+#    #+#             */
+/*   Updated: 2024/05/26 22:47:03 by elsurovt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    size_t i;
+    char *str;
+
+    if (!s)
+        return (NULL);
+    str = (char *)malloc(sizeof(char) * (len + 1));
+    if (!str)
+        return (NULL);
+    i = 0;
+    while ((len > i) && s[start + i] && (start < ft_strlen((char *)s)))
+    {
+        str[i] = s[start + i];
+        i++;
+    }
+    str[i] = '\0';
+    return (str);
+}
