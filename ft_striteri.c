0e426ca8b04d2804fa49d29fb34739af4513b7fc
Ellie <102157178+Otazka@users.noreply.github.com>
2024-05-29 08:03:23 +0200

    striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -14,12 +14,12 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-    unsigned int	i;
+    size_t	i;
 
-    i = 0;
-    while (s[i] != '\0')
+    if(s && f)
     {
-        f(i, s + i);
-        i++;
+        i = 0;
+        while (*s)
+            f(i++, s++);
     }
 }
