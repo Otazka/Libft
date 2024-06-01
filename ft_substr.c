51f89f0d9ff5c0a59f6ef414e9629b5eaffec304
Elena Surovtseva <elsurovt@c2r5s3.42prague.com>
2024-06-01 13:23:34 +0200

    fixed

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: elsurovt <elsurovt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 22:32:10 by elsurovt          #+#    #+#             */
-/*   Updated: 2024/06/01 13:12:25 by elsurovt         ###   ########.fr       */
+/*   Updated: 2024/06/01 13:22:46 by elsurovt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,25 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
+	size_t	str_len;
+	size_t	i;
 
 	if (!s)
 		return (NULL);
-	if (ft_strlen(s) < start)
-		len = 0;
-	if (ft_strlen(s + start) < len)
-		len = ft_strlen(s + start);
-	str = malloc(sizeof(char) * (len + 1));
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		return (ft_strdup(""));
+	if (str_len < start + len)
+		len = str_len - start;
+	str = (char *)malloc(len + 1);
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, s + start, len + 1);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
 	return (str);
 }
